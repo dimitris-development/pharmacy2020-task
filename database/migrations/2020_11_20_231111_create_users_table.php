74df8fe9b3ba73d8c1e8ae523101d91a6ee9dd3c
James C <dimitrios.collier@gmail.com>
2020-11-26 06:31:24 +0200

    Added indexes

--- database/migrations/2020_11_20_231111_create_users_table.php
+++ database/migrations/2020_11_20_231111_create_users_table.php
@@ -14,10 +14,11 @@ class CreateUsersTable extends Migration {
         
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('password');
+            $table->index(['email', 'password']);
         });
 
     }
