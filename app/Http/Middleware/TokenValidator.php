55f7d19897578ad0b99946fa2557d46bc972b2cb
James C <dimitrios.collier@gmail.com>
2020-12-16 22:43:05 +0200

    Fixed wrong HTTP Status Code on token validation and added WWW-Authenticate header as per RFC6750

--- app/Http/Middleware/TokenValidator.php
+++ app/Http/Middleware/TokenValidator.php
@@ -26,12 +26,25 @@ class TokenValidator
      */
     public function handle($request, Closure $next) : JsonResponse
     {
+
         $bearer_token = $request->bearerToken();
         $validator_resp = TokenController::validateToken($bearer_token);
-        if (['message' => 'Token validated'] === $validator_resp){
-            return $next($request);
+
+        if (['message' => 'Token validated'] === $validator_resp) {
+            $response = $next($request);
+        } elseif (in_array(['error_id' => 3], $validator_resp, true)) {
+            $response = response()->json($validator_resp, 401)
+                    ->header('WWW-Authenticate',
+                    '
+                            Bearer error=\''.$validator_resp['error'].'\'
+                            error_description=\''.$validator_resp['error_description'].'\'
+                          '
+                    );
+        } else {
+            $response = response()->json($validator_resp, 403);
         }
-        return response()->json($validator_resp, 401);
+
+        return $response;
     }
 
 }
